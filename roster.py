9c04fbff3c1f8503765c437137128d56c67b7142
Jaik Prasad <jaiki@umich.edu>
2018-09-26 15:25:33 -0400

    Create roster.py
    Created roster.py, with instructions at top and all registered student names in class arranged alphabetically, by last name.

---
+++ roster.py
@@ -0,0 +1,88 @@
+#Class roster for FIN 342: Big Data in Finance
+
+#This roster is alphabetical (by last name), and will include the following information for each student:
+  
+    # Program (i.e. BBA, MBA)
+    # uniquename
+    # Favorite class in college (i.e. COMPLXSYS 391)
+    # Favorite food
+    
+#To make things a bit easier, names are already alphabetically sorted. 
+
+# Marius Ahlgrimm
+
+# Miguel Andrade
+
+# Novonil Banik
+
+# Jakob Boegsted
+
+# Adam Cooper
+
+# Evan Fisher
+
+# Kwun Fung Fung
+
+# Peter Gaffney Jr
+
+# Noah Gilbert
+
+# Teodoro De Giuli
+
+# Jack Groom
+
+# Shabir Ahmad Hameedi
+
+# Archibald Hauck
+
+# Jun Bum Kim
+
+# Michael Kim
+
+# Jashan Daniel Kishore
+
+# Anurag Koduri
+
+# Kyle Koshiyama
+
+# Jacob Kreinik
+
+# Grace Liu
+
+# Jason Miller
+
+# Tyler Mulier
+
+# Cameron Newman
+
+# Connor Page
+
+# Jake Pothast
+
+# Blake Rogow
+
+# Sloane Rubin
+
+# Matthew Sattam
+
+# Daniel Stockton
+
+# Michael Tang
+
+# Wariya Thaweekitkasem
+
+# Andrew Todd
+
+# Prashant Toteja
+
+# Benjamin Wachtel
+
+# Paige Wiersema
+
+# Sven Wollschlaeger
+
+# Chun Wu
+
+# Yuting Wu
+
+# Kevin Yu
